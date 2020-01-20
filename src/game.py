3ca7487e22da07bbdecbcd4e615d79c531306340
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2020-01-20 22:47:58 +0530

    game starting to make sense after all

--- src/game.py
+++ src/game.py
@@ -1,2 +1,20 @@
 class game:
-    
+    def run_loop:
+    	while true:
+
+    		print_board()
+    		'''
+    		take input 
+    		as per input, change the accelaration of the player
+
+    		and initialise any new objects ( say bullets , jet etc and whatever drawings need to be drawn)
+
+    		'''
+
+
+    		'''
+				and now that all the available accelerations are there for every active object, organise for them to move around, 
+
+				once the small scale movements have happenned, check for any collisons and update the object statuses accordingly 
+
+			'''
